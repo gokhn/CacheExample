e65f2cf1b1b7c9f29839182094170325c642de0f
gokhngungor <gokhngungor@gmail.com>
2020-10-29 15:44:39 +0300

    add MemoryCache

--- RedisExample/Startup.cs
+++ RedisExample/Startup.cs
@@ -26,6 +26,7 @@ namespace RedisExample
             services.AddControllersWithViews();
 
             services.AddMemoryCache();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
