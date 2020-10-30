40c20f35f6f0ba48a59665e230b0f2339a549580
gokhngungor <gokhngungor@gmail.com>
2020-10-30 08:24:11 +0300

    .

--- RedisExample/Controllers/PersonelController.cs
+++ RedisExample/Controllers/PersonelController.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace RedisExample.Controllers
 {
     public class PersonelController : Controller
     {
+        IMemoryCache _memoryCache;
+        public PersonelController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
         public IActionResult Index()
         {
+
+            _memoryCache.Set("PersonName", "GokhanG");
+
+            string name = _memoryCache.Get<string>("PersonName");
+
+            return View();
+        }
+
+        public IActionResult SetCache()
+        {
+            _memoryCache.Set("PersonName", "GokhanG");
+            return View();
+        }
+
+        public IActionResult GetCache()
+        {
+            string name = _memoryCache.Get<string>("PersonName");
             return View();
         }
     }
